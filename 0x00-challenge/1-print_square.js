82e02517fe907d93a0c88f444e0ebcce6281db35
SPaci20 <spaci20@gmail.com>
2023-10-19 20:48:52 +0200

    Print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2]);
 
 for (let i = 1 ; i <= size ; i ++) {
     for (let j = 1 ; j <= size ; j ++) {
