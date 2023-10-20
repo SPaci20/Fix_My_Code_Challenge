5b56da20624823fd328b069eb0dd1de01f89c358
SPaci20 <spaci20@gmail.com>
2023-10-20 19:50:22 +0200

    Double linked list 2

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,3 +1,6 @@
+#include "lists.h"
+#include <stdlib.h>
+
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
@@ -8,34 +11,29 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	if (*head == NULL)
+		return (-1);
+
 	dlistint_t *current = *head;
+	unsigned int count = 0;
 
-	if (!*head)
-		return (-1);
-
-	if (index == 0)
+	while (count < index && current != NULL)
 	{
-		*head = current->next;
-		if (*head)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
-	}
-
-	for (unsigned int i = 0; current && i < index; i++)
-	{
-		if (!current->next)
-			return (-1);
 		current = current->next;
+		count++;
 	}
 
-	if (!current)
+	if (current == NULL)
 		return (-1);
 
-	current->prev->next = current->next;
-	if (current->next)
-		current->next->prev = current->prev;
-	free(current);
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	else
+		*head = current->next;
 
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
+	free(current);
 	return (1);
 }
